a4a16b2e573d3468b87755f8378395abcd39dd48
Nurulloh <nurulloh@mysuperdispatch.com>
2020-07-08 16:07:19 +0500

    Initial commit

---
+++ app/src/main/java/uz/mirsaidoff/plandaydemo/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package uz.mirsaidoff.plandaydemo.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
